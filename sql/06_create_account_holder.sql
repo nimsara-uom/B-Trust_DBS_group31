6f76140e946c441af90873212e133e42ffa5c3f4
nimsara-uom <kodithuwakkukann.24@uom.lk>
2026-09-12 19:18:21 +0530

    Commit 06:add ACCOUNTHOLDER junction table (with joint accts)

---
+++ sql/06_create_account_holder.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS ACCOUNTHOLDER (
+    account_id  INT NOT NULL,  
+    customer_id INT NOT NULL,   
+
+    --these two form the primary key
+
+    CONSTRAINT pk_account_holder PRIMARY KEY (account_id, customer_id),
+    CONSTRAINT fk_holder_account
+        FOREIGN KEY (account_id)
+        REFERENCES SAVINGSACCOUNT (account_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,   
+
+    CONSTRAINT fk_holder_customer
+        FOREIGN KEY (customer_id)
+        REFERENCES CUSTOMER (customer_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT   
+);
