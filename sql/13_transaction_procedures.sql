634a2004844eaab6bfd86c998162334082aa9b84
Raveesha Sandamini <raveeshasandamini20041208@gmail.com>
2026-09-16 22:27:10 +0530

    add withdrawal stored procedure

--- sql/13_transaction_procedures.sql
+++ sql/13_transaction_procedures.sql
@@ -23,5 +23,28 @@ BEGIN
 END //
 
 
+-- Process a Withdrawal
+CREATE PROCEDURE sp_ProcessWithdrawal(
+    IN p_AccountID INT,
+    IN p_AgentID INT,
+    IN p_Amount DECIMAL(12, 2),
+    IN p_ReferenceNo VARCHAR(30)
+)
+BEGIN
+    -- Start the ACID transaction
+    START TRANSACTION;
+
+    -- Insert the record. Triggers will block overdrafts and update the balance.
+    INSERT INTO BANK_TRANSACTION (
+        account_id, agent_id, reference_no, transaction_type, amount, transaction_timestamp
+    ) VALUES (
+        p_AccountID, p_AgentID, p_ReferenceNo, 'Withdrawal', p_Amount, NOW()
+    );
+
+    -- Save the changes permanently
+    COMMIT;
+END //
+
+
 
 DELIMITER ;
