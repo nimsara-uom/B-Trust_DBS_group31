b035503a2c0e235c86a04c10088d3f07c290c6c8
Lakshan Jayawardana <lakshan23sj@gmail.com>
2026-09-23 22:51:50 +0530

    update: add FD ID, account number, and customer name to interest report

--- sql/report_4_monthly_interest_distribution.sql
+++ sql/report_4_monthly_interest_distribution.sql
@@ -1,15 +1,26 @@
 select 
     year(tr.transaction_timestamp) as year,
     month(tr.transaction_timestamp) as month,
+    fd.fd_id as fd_id,
+    sa.account_number as account_number,
+    c.full_name as customer_name,
     concat(fp.term_months, ' months') as plan_name,
     count(tr.transaction_id) as number_of_interest_creadits,
     sum(tr.amount) as total_inerest_creadited
 from bank_transaction tr
     join fixeddeposit fd on fd.fd_id = tr.fd_id
+    join savingsaccount sa on sa.account_id = fd.account_id
+    join accountholder a on a.account_id = sa.account_id
+    join customer c on a.customer_id = c.customer_id
     join fdplan fp on fp.fd_plan_id = fd.fd_plan_id
     where tr.transaction_type = 'FD_Interest'
 group by 
     year(tr.transaction_timestamp), 
     month(tr.transaction_timestamp),
-    concat(fp.term_months, ' months')
+    concat(fp.term_months, ' months'),
+    fd.fd_id,
+    sa.account_number,
+    c.full_name,
+    fp.fd_plan_id,
+    fp.term_months
 order by year(tr.transaction_timestamp) desc, month(tr.transaction_timestamp) desc;
