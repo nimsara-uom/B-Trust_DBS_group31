61d3e871cb4b14a3f2db7d88fe2c6ea28b8e341d
Lakshan Jayawardana <lakshan23sj@gmail.com>
2026-09-23 22:28:21 +0530

    update: include customer name in savings transaction summary

--- sql/report_2_account_transaction_summary.sql
+++ sql/report_2_account_transaction_summary.sql
@@ -1,11 +1,14 @@
 select
     sa.account_number as account_number,
+    c.full_name as customer_name,
     sp.plan_name as plan_name,
     count(tr.account_id) as number_of_transactions,
     sum(tr.amount) as total_amount,
     sa.current_balance as current_balance
 from savingsaccount sa 
+    join accountholder a on a.account_id = sa.account_id
+    join customer c on a.customer_id = c.customer_id
     join savingsplan sp on sa.plan_id = sp.plan_id
     join bank_transaction tr on sa.account_id = tr.account_id
-group by sa.account_id
+group by sa.account_id, c.customer_id
 order by count(tr.account_id) desc;
