810daf6e91c70a23595ec50dfe7f82e5dd4d0272
Lakshan Jayawardana <lakshan23sj@gmail.com>
2026-09-23 22:34:57 +0530

    update: include customer name and interest rate in FD report

--- sql/report_3_active_fds_and_next_interest_payout.sql
+++ sql/report_3_active_fds_and_next_interest_payout.sql
@@ -2,11 +2,15 @@ select
     fd.fd_id as fd_id,
     sa.account_number as account_number,
     concat(fp.term_months, ' months') as plan_name,
+    c.full_name as customer_name,
     fd.principal_amount as principal_amount,
+    fp.interest_rate as interest_rate,
     fd.start_date as start_date,
     fd.maturity_date as maturity_date,
     fd.next_interest_date as next_interest_date
 from fixeddeposit fd
+    join accountholder a on a.account_id = fd.account_id
+    join customer c on c.customer_id = a.customer_id
     join savingsaccount sa on sa.account_id = fd.account_id
     join fdplan fp on fp.fd_plan_id = fd.fd_plan_id
     where fd.status = 'Active'
